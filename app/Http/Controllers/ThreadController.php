01cf86067d0d736286741aef7df5127759619573
CORPNET\suman.teraiya <suman.teraiya@devitpl.com>
2024-09-03 11:31:05 +0530

    Initial commit

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Illuminate\Http\Request;
+
+class ThreadController extends Controller
+{
+    //
+}
+
+
